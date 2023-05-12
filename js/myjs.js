ba3c8cac1cbf6db6c1167d343ac77949f987a1fa
gineth22 <getrillosp@ufpso.edu.co>
2023-05-12 15:38:35 -0500

    add media query

--- js/myjs.js
+++ js/myjs.js
@@ -1,1 +1,19 @@
-alert("hola bienvenido")
+let enlaces_header = document.querySelectorAll(".enlaces-header")[ ]
+let icon_hamburguer = true;
+
+document.querySelectorAll(".hamburguer")[ 0].addEventListener("clik", function()){
+    if(icon_hamburguer){
+        document.querySelectorAll(".hamburguer")[ 0].style.color="#fff";
+        icon_hamburguer= false;
+    }else{
+    document.querySelectorAll("hamburguer")[0].style.color="#000";
+    icon_hamburguer = true;
+    }
+    enlaces-Header.classlist.toggle("hamburgueropen");
+}
+
+
+document.querySelectorAll9(".enlaces-header")[0].addEventListener ("click" , function)¨{
+    enlaces_header.classList.toggle("hamburgueropen");
+    documentquery
+}
